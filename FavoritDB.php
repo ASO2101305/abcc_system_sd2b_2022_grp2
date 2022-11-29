3ad3eb8e6fd897c6c3037e5bb0178b6e206f4612
ASO2201114 <114889606+ASO2201114@users.noreply.github.com>
2022-11-29 13:32:21 +0900

    Update FavoritDB.php

--- FavoritDB.php
+++ FavoritDB.php
@@ -1,6 +1,17 @@
 <?php
-require_once "DBManager.php";
-require_once "list.php";
+require_once 'DBManager.php';
+require_once 'list.php';
+$dbmng = new DBManager();
 
-if()
+public function FavoritDB($getflag,$getclient,$getproduct){
+    if($_POST['flag'] == "INSERT"){
+        $sql = "INSERT INTO favorit(favorit_id,client_id,product_id) VALUES (1,$getclient,$getproduct)";
+        $ps=$pdo->prepare($sql);
+        $ps->execute();
+    }else{
+        $sql = "DELETE FROM favorit WHERE product_id = $getproduct"; 
+        $ps=$pdo->prepare($sql);
+        $ps->execute();
+    }
+}
 ?>
