f36f91d0bcf2a2a3dce956db13dd36089f89a0c5
Dusty502 <99384009+Dusty502@users.noreply.github.com>
2022-11-29 17:11:08 +0900

    Create InsertCart.php

---
+++ InsertCart.php
@@ -0,0 +1,18 @@
+<?php
+    public function InsertCart(){
+    $id = isset($_POST['id'])? htmlspecialchars($_POST['id'], ENT_QUOTES, 'utf-8') : '';
+    $name = isset($_POST['name'])? htmlspecialchars($_POST['name'], ENT_QUOTES, 'utf-8') : '';
+    $price = isset($_POST['price'])? htmlspecialchars($_POST['price'], ENT_QUOTES, 'utf-8') : '';
+    $count = isset($_POST['count'])? htmlspecialchars($_POST['count'], ENT_QUOTES, 'utf-8') : '';
+
+
+    require_once "sessioncheck.php";
+//配列に入れるには、$name,$count,$priceの値が取得できていることが前提なのでif文で空のデータを排除する
+    if($name!=''&&$count!=''&&$price!=''){
+            $_SESSION['products'][$id]=[
+                 'count' => $count,
+                 'price' => $price
+            ];
+       }
+    }
+?>
