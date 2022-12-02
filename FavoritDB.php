3ebd7cee238d4627a34eefbdc959407c31e2de01
ASO2201114 <114889606+ASO2201114@users.noreply.github.com>
2022-12-02 09:59:07 +0900

    Update FavoritDB.php

--- FavoritDB.php
+++ FavoritDB.php
@@ -1,17 +1,30 @@
 <?php
 require_once 'DBManager.php';
 require_once 'list.php';
-$dbmng = new DBManager();
-
-public function FavoritDB($getflag,$getclient,$getproduct){
-    if($_POST['flag'] == "INSERT"){
-        $sql = "INSERT INTO favorit(favorit_id,client_id,product_id) VALUES (1,$getclient,$getproduct)";
-        $ps=$pdo->prepare($sql);
-        $ps->execute();
-    }else{
-        $sql = "DELETE FROM favorit WHERE product_id = $getproduct"; 
-        $ps=$pdo->prepare($sql);
-        $ps->execute();
+class Favorit{
+    public function InsertFavoritDB($getflag,$getclient,$getproduct){
+        $dbmng = new DBManager;
+        $pdo = $dbmng-> dbConnect();
+        if($_POST['flag'] == "INSERT"){
+            $sql = "INSERT INTO favorit(favorit_id,client_id,product_id) VALUES (1,$getclient,$getproduct)";
+            $ps=$pdo->prepare($sql);
+            $ps->execute();
+        }else{
+            $sql = "DELETE FROM favorit WHERE product_id = $getproduct"; 
+            $ps=$pdo->prepare($sql);
+            $ps->execute();
+        }
     }
+    public function SelsectFavoritPdc(){
+        $dbmng = new DBManager;
+        $pdo = $dbmng-> dbConnect();
+        $sql = "SELECT * FROM favorit";
+        $ps = $pdo->prepare($sql);
+        $ps->execute();
+        $Fitem = $ps->fetchAll();
+        return $Fitem;
 }
+}
+
+
 ?>
