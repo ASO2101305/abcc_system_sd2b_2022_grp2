6008a27f54e18f48b5acdb58a4494a2e4b824784
ASO2201114 <114889606+ASO2201114@users.noreply.github.com>
2022-12-05 10:14:38 +0900

    Create Navmodel.php

---
+++ Navmodel.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Work<br>buddy</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample02">
+      <form>
+          <input class="form-control" type="text" placeholder="商品名を入力" name="pdcname">
+          <input class="" formtype="submit" value="検索">
+        </form>
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <class
+            <a class="nav-link" href="#">カート</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
