30aad93f41b49a790b352f0b6860b5ce337668a8
Dusty502 <99384009+Dusty502@users.noreply.github.com>
2022-12-05 10:40:14 +0900

    Create session.php

---
+++ php/session.php
@@ -0,0 +1,15 @@
+<!--ログインしてセッションに格納する-->
+<?php 
+    session_start();
+    require_once "DBManager.php";
+    $dbm = new DBManager();
+    $userData = $dbm->checkLoginByMailAndPass($_POST['mail'],$_POST['pass']);
+    foreach($userData as $row){
+        $_SESSION['mailaddress'] = $row['mail_address'];
+        $_SESSION['pass'] = $row['customer_pass'];
+        header('Location: list.php');
+    }
+    if(count($userData)==0){
+        header('Location:login.php');
+    }
+?>
