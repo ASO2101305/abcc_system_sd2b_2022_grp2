ee0e72901393581927ba389065ff0826041b3283
ASO2201114 <114889606+ASO2201114@users.noreply.github.com>
2022-12-07 12:59:57 +0900

    Update SearchPdc.php

--- php/SearchPdc.php
+++ php/SearchPdc.php
@@ -7,17 +7,42 @@ class search{
         $sql = "SELECT * FROM Product ORDER BY sale_date DESC";
         $ps = $PDO->prepare($sql);
         $ps->execute();
-        $ArrayPdc = $ps->fetchAll();
+        $results = $ps->fetchAll();
+
+        $ArrayPdc = array();
+        foreach($results as $result){
+            $product = new Product();
+            $product->product_id = $result['product_id'];
+            $product->product_name = $result['product_name'];
+
+            $product->source = $result['source'];
+            // 全部代入する
+
+            // リターンする配列に追加
+            $ArrayPdc[ ] = $product;
+        }
         return $ArrayPdc;
     }
     
-        function SearchPdcByLogs(){
+    function SearchPdcByLogs(){
         $pdo = new DBManager;
         $PDO = $pdo->dbConnect();
-        $sql = "SELECT * FROM order_logs ORDER BY order_date DESC";
+        $sql = "SELECT * FROM order_log ORDER BY order_date DESC";
         $ps = $PDO->prepare($sql);
         $ps->execute();
         $Arraylogs = $ps->fetchAll();
         return $Arraylogs;
     }
 }
+
+class Product {
+    public $product_id;
+    public $product_bunrui_id;
+    public $product_name;
+    public $product_price;
+    public $function_detail;
+    public $size;
+    public $sale_date;
+    public $maker_id;
+    public $source;
+}  
